2501288f9179a688c627f7dad94c20e35e685486
Xitaymin <y.barashenko@gmail.com>
2021-09-05 14:04:12 +0300

    added update task feature

--- src/main/java/servlet/MainServlet.java
+++ src/main/java/servlet/MainServlet.java
@@ -34,36 +34,47 @@ public class MainServlet extends HttpServlet {
     }
 
     @Override
-    //POST /todo в теле запроса JSON с задачей, схема ниже. если id объявлен - заменить ассоциированную с ним.
-    //Если id в теле запроса отсутствует или null, сгенерировать id и сохранить задачу как новую. Результатом вернуть JSON с сохраненной задачей.
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        String body = req.getReader().lines().reduce("", (accumulator, actual) -> accumulator + actual);
+    //POST /todo в теле запроса JSON с задачей, схема ниже. если id объявлен
+    // - заменить ассоциированную с ним.
+    //Если id в теле запроса отсутствует или null, сгенерировать id и
+    // сохранить задачу как новую. Результатом вернуть JSON с сохраненной
+    // задачей.
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        //        String body = req.getReader().lines().reduce("",
+        //        (accumulator, actual) -> accumulator + actual);
         req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
         String body = req.getReader().lines().collect(Collectors.joining());
         System.out.println(body);
 
-        ObjectMapper mapper = new ObjectMapper()
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        ObjectMapper mapper =
+                new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-        Task task = mapper.readValue(body,Task.class);
-        //todo set correct encoding
-        if(task.getId()==0) {
+        Task task = mapper.readValue(body, Task.class);
+        //todo set encoding in filter
+        if (task.getId() == 0) {
             task.setId(body.hashCode());
 
             tasks.put(task.getId(), task);
             System.out.println(task);
 
-            resp.setCharacterEncoding("UTF-8");
-
             PrintWriter out = resp.getWriter();
             resp.setContentType("application/json");
             out.print(mapper.writeValueAsString(task));
             out.flush();
+            out.close();
+        } else {
+            tasks.replace(task.getId(), task);
+            PrintWriter out = resp.getWriter();
+            resp.setContentType("application/json");
+            out.print(mapper.writeValueAsString(task));
+            out.flush();
+            out.close();
         }
     }
 
     @Override
-    public void init() throws ServletException {
-    tasks = new HashMap<>();
+    public void init() {
+        tasks = new HashMap<>();
     }
 }
